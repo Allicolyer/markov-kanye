bb39cf82c713c925eb4184cf844f032f013d5d55
Alli Colyer <allicolyer@gmail.com>
2020-01-23 13:56:13 -0500

    add slider toggle css and javasript, add spacing classes, clean up spacing

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,25 @@ import { kanye_quotes } from "./formatted-quotes.js";
 
 const first_order_map = generate_map(kanye_quotes, 1);
 const second_order_map = generate_map(kanye_quotes, 2);
+let map = second_order_map;
 
+//wait until the DOM is loaded before adding event listeners
 document.addEventListener("DOMContentLoaded", function() {
-  // const first_order_quote = document.getElementById("first_order_quote");
+  const checkbox = document.querySelector("input[name=randomness]");
   const quote_text = document.getElementById("quote_text");
   const quote_button = document.getElementById("quote_button");
 
+  //toggles between the first order and second order markov chains
+  checkbox.addEventListener("change", function() {
+    if (this.checked) {
+      map = first_order_map;
+    } else {
+      map = second_order_map;
+    }
+  });
+
+  //Generate new setnence
   quote_button.onclick = () => {
-    // first_order_quote.innerHTML = generate_sentence(map);
-    quote_text.innerHTML = generate_sentence(second_order_map);
+    quote_text.innerHTML = generate_sentence(map);
   };
 });
