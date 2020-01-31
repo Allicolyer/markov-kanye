9237553f738a87e3dd28e7f70023f6749e8a9caf
Alli Colyer <allicolyer@gmail.com>
2020-01-30 21:06:47 -0500

    use API end point, remove Javascript files

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,25 @@
-import { generate_map, generate_sentence } from "./markov.js";
-import { kanye_quotes } from "./formatted-quotes.js";
-
-const first_order_map = generate_map(kanye_quotes, 1);
-const second_order_map = generate_map(kanye_quotes, 2);
-let map = second_order_map;
-
 //wait until the DOM is loaded before adding event listeners
 document.addEventListener("DOMContentLoaded", function() {
   const checkbox = document.querySelector("input[name=randomness]");
   const quoteText = document.getElementById("quote-text");
   const quoteButton = document.getElementById("quote_button");
 
-  //toggles between the first order and second order markov chains
+  //set the first order for the Markov chain to be 2
+  const order = 2;
+
+  //Toggles between the first order and second order Markov chains
   checkbox.addEventListener("change", function() {
     if (this.checked) {
-      map = first_order_map;
+      order = 1;
     } else {
-      map = second_order_map;
+      order = 2;
     }
   });
 
-  //Generate new setnence
+  //Generate a new quote
   quoteButton.onclick = () => {
-    quoteText.innerHTML = generate_sentence(map);
+    fetch(`https://kanyefun.now.sh/sentence/${order}`)
+      .then(response => response.json())
+      .then(res => (quoteText.innerHTML = res));
   };
 });
