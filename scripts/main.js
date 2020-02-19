96ac7832a22edc23ce10dd8c193e97c8fbba7a43
Alli Colyer <allicolyer@gmail.com>
2020-02-19 17:41:01 -0500

    add javascript to remove fbclid URL parameter

--- scripts/main.js
+++ scripts/main.js
@@ -25,3 +25,21 @@ document.addEventListener("DOMContentLoaded", function() {
     quoteText.innerHTML = generateQuote(markovChain);
   };
 });
+
+//removes fbclid parameter added by facebook sharing: https://www.michalspacek.com/using-javascript-to-modify-urls-and-hide-fbclid
+(function() {
+  var param = "fbclid";
+  if (location.search.indexOf(param + "=") !== -1) {
+    var replace = "";
+    try {
+      var url = new URL(location);
+      url.searchParams.delete(param);
+      replace = url.href;
+    } catch (ex) {
+      var regExp = new RegExp("[?&]" + param + "=.*$");
+      replace = location.search.replace(regExp, "");
+      replace = location.pathname + replace + location.hash;
+    }
+    history.replaceState(null, "", replace);
+  }
+})();
